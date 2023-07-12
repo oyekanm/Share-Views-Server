59f385e34485dfa5975344fbed481a4773b78a17
Oyekanmi Boluwatife <enitanboluwatife5@gmail.com>
2023-07-12 12:42:10 +0100

    new commit

--- index.js
+++ index.js
@@ -1,32 +1,38 @@
 const express = require("express");
-const dotenv = require('dotenv')
-const cors = require("cors")
+const dotenv = require("dotenv");
+const cors = require("cors");
 
 const db = require("./models");
 const router = require("./routes/PostRoute");
 const commentRouter = require("./routes/CommentsRoute");
 const UsersRouter = require("./routes/UsersRoute");
 const LikesRouter = require("./routes/LikesRoute");
-const {NotFound,ErrorBoundary} = require("./middleware/errorMiddleware");
+const { NotFound, ErrorBoundary } = require("./middleware/errorMiddleware");
 
-dotenv.config()
+dotenv.config();
 const port = process.env.PORT;
 
-const app = express()
-// db.sequelize.sync()
+const app = express();
 
-app.use(express.json())
-app.use(cors())
-app.use("/api/posts",router) 
-app.use("/api/comments",commentRouter) 
-app.use("/api/auth",UsersRouter) 
-app.use("/api/likes",LikesRouter) 
+app.use(express.json());
+app.use(cors());
+app.use("/api/posts", router);
+app.use("/api/comments", commentRouter);
+app.use("/api/auth", UsersRouter);
+app.use("/api/likes", LikesRouter);
 
 // console.log(port);
 
-app.listen(port,()=>{
-    console.log(process.env.NODE_ENV  + port);
-})
+db.sequelize
+  .sync()
+  .then(() => {
+    app.listen(port, () => {
+      console.log(process.env.NODE_ENV + port);
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
-app.use(NotFound)
-app.use(ErrorBoundary)
+app.use(NotFound);
+app.use(ErrorBoundary);
