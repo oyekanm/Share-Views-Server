f435da2c84cf228f5e3feea29ff19a8edb4f0248
Oyekanmi Boluwatife <enitanboluwatife5@gmail.com>
2023-07-12 11:45:00 +0100

    new commit

--- config/config.js
+++ config/config.js
@@ -24,7 +24,7 @@ module.exports = {
     "production": {
       "username": "root",
       "password": process.env.DB_PASSWORD,
-      "database": "Viewsdb",
+      "database": "railway",
       "host": process.env.DB_HOST,
       "dialect": "mysql"
     }
